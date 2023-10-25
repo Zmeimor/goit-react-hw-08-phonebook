d640e2b7cab642fa92502776e995b7d6888029d7
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-10-25 09:27:05 +0200

    пп

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 //  { store, persistor } from './components/redux/store';
 import { store } from './components/redux/store';
@@ -12,9 +12,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <App />
+    </BrowserRouter>
   </Provider>
   // </React.StrictMode>
 );
